5c08fec776bd7107749483eca83e90143afc5195
José Daniel Penin Lovera <jose.penin.lovera@gmail.com>
2020-04-29 12:13:53 -0300

    agrego async a la funcion

--- express/server.js
+++ express/server.js
@@ -1,42 +1,41 @@
-'use strict';
-const express = require('express');
-const path = require('path');
-const serverless = require('serverless-http');
+"use strict";
+const express = require("express");
+const path = require("path");
+const serverless = require("serverless-http");
 const app = express();
-const bodyParser = require('body-parser');
-const sendgrid = require('@sendgrid/mail')
+const bodyParser = require("body-parser");
+const sendgrid = require("@sendgrid/mail");
 
-sendgrid.setApiKey(process.env.SENDGRID_API_KEY)
+sendgrid.setApiKey(process.env.SENDGRID_API_KEY);
 
 const router = express.Router();
-router.get('/', (req, res) => {
-  res.writeHead(200, { 'Content-Type': 'text/html' });
-  res.write('<h1>Hello from Express.js!</h1>');
+router.get("/", (req, res) => {
+  res.writeHead(200, { "Content-Type": "text/html" });
+  res.write("<h1>Hello from Express.js!</h1>");
   res.end();
 });
-router.get('/another', (req, res) => {
+router.get("/another", async (req, res) => {
   const msg = {
-    to: 'pepe81@msn.com',
-    from: 'administrador@desarrolladorweb.net',
-    subject: 'Sending with Twilio SendGrid is Fun',
-    text: 'and easy to do anywhere, even with Node.js',
-    html: '<strong>and easy to do anywhere, even with Node.js</strong>',
-  }
+    to: "pepe81@msn.com",
+    from: "administrador@desarrolladorweb.net",
+    subject: "Sending with Twilio SendGrid is Fun",
+    text: "and easy to do anywhere, even with Node.js",
+    html: "<strong>and easy to do anywhere, even with Node.js</strong>",
+  };
   try {
     await sendgrid.send(msg);
   } catch (error) {
     return res.status(error.statusCode || 500).json({ error: error.message });
   }
 
-  res.sendStatus(200)
+  res.sendStatus(200);
+});
 
-})
-
-router.post('/', (req, res) => res.json({ postBody: req.body }));
+router.post("/", (req, res) => res.json({ postBody: req.body }));
 
 app.use(bodyParser.json());
-app.use('/.netlify/functions/server', router);  // path must route to lambda
-app.use('/', (req, res) => res.sendFile(path.join(__dirname, '../index.html')));
+app.use("/.netlify/functions/server", router); // path must route to lambda
+app.use("/", (req, res) => res.sendFile(path.join(__dirname, "../index.html")));
 
 module.exports = app;
 module.exports.handler = serverless(app);
